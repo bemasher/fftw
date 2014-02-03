06f5af5dc98e1145f6066ae07f584543b5cdc89b
BeMasher <bemasher@gmail.com>
2014-02-03 12:09:45 -0700

    Added comments.

--- fftw.go
+++ fftw.go
@@ -16,14 +16,19 @@ type plan struct {
 	p C.fftw_plan
 }
 
+// The user is required to close created plans, this points to data
+// allocated in C which Go GC is unaware of.
 func (p plan) Close() {
 	C.fftw_destroy_plan(p.p)
 }
 
+// Execute the planned transform using data contained in the arrays allocated
+// during planning.
 func (p plan) Execute() {
 	C.fftw_execute(p.p)
 }
 
+// 1D DFT for transforming complex data to complex data.
 type DFT1DPlan struct {
 	plan
 	Direction Direction
@@ -32,11 +37,14 @@ type DFT1DPlan struct {
 	In, Out   []complex128
 }
 
+// Plans 1D DFT of size n.
 func NewDFT1D(n uint, dir Direction, locality Locality, planFlags PlanFlag) (plan DFT1DPlan) {
+	// Store configuration for string formatting later
 	plan.Direction = dir
 	plan.Locality = locality
 	plan.PlanFlags = planFlags
 
+	// Allocate the input and output arrays according to given locality.
 	plan.In = make([]complex128, n)
 	switch locality {
 	case InPlace:
@@ -47,6 +55,7 @@ func NewDFT1D(n uint, dir Direction, locality Locality, planFlags PlanFlag) (pla
 		panic(fmt.Sprintf("invalid locality: %d", locality))
 	}
 
+	// Create the plan using the given arrays and flags.
 	plan.plan.p = C.fftw_plan_dft_1d(
 		C.int(len(plan.In)),
 		(*C.fftw_complex)(&plan.In[0]),
@@ -57,6 +66,7 @@ func NewDFT1D(n uint, dir Direction, locality Locality, planFlags PlanFlag) (pla
 	return
 }
 
+// 1D Real-to-Complex DFT for transforming real data to complex data.
 type DFTR2CPlan struct {
 	plan
 	Locality  Locality
@@ -65,31 +75,34 @@ type DFTR2CPlan struct {
 	Out       []complex128
 }
 
-func (r2c DFTR2CPlan) String() string {
-	return fmt.Sprintf("{Kind:R2C Locality:%s PlanFlags:%s In:[%d]complex128 Out:[%d]float64}", r2c.Locality, r2c.PlanFlags, len(r2c.In), len(r2c.Out))
-}
-
+// Plans 1D Real-to-Complex DFT of size n. For in-place transforms input and
+// output arrays are backed by the same array using reflection. This is
+// inherently unsafe as it requires casting an array of one type to another
+// which is not allowed in the type system.
 func NewDFTR2C(n uint, locality Locality, planFlags PlanFlag) (plan DFTR2CPlan) {
 	plan.Locality = locality
 	plan.PlanFlags = planFlags
 
-	outLen := int(n>>1 + 1)
+	plan.In = make([]float64, n)
 
+	// Allocate the input and output arrays according to given locality. In-
+	// place transforms require reflection to point the input and output
+	// arrays to the same data since they are of different types.
 	switch locality {
 	case InPlace:
 		plan.In = make([]float64, n+2)
 		header := *(*reflect.SliceHeader)(unsafe.Pointer(&plan.Out))
-		header.Len = outLen
-		header.Cap = outLen
+		header.Len = int(n>>1 + 1)
+		header.Cap = header.Len
 		header.Data = uintptr(unsafe.Pointer(&plan.In[0]))
 		plan.Out = *(*[]complex128)(unsafe.Pointer(&header))
 	case OutOfPlace:
-		plan.In = make([]float64, n)
-		plan.Out = make([]complex128, outLen)
+		plan.Out = make([]complex128, n>>1+1)
 	default:
 		panic(fmt.Sprintf("invalid locality: %d\n", locality))
 	}
 
+	// Create the plan using the given arrays and flags
 	plan.plan.p = C.fftw_plan_dft_r2c_1d(
 		C.int(n),
 		(*C.double)(&plan.In[0]),
@@ -100,6 +113,11 @@ func NewDFTR2C(n uint, locality Locality, planFlags PlanFlag) (plan DFTR2CPlan) 
 	return
 }
 
+func (r2c DFTR2CPlan) String() string {
+	return fmt.Sprintf("{Kind:R2C Locality:%s PlanFlags:%s In:[%d]complex128 Out:[%d]float64}", r2c.Locality, r2c.PlanFlags, len(r2c.In), len(r2c.Out))
+}
+
+// 1D Complex-to-Real DFT for transforming complex data to real data.
 type DFTC2RPlan struct {
 	plan
 	Locality  Locality
@@ -108,21 +126,24 @@ type DFTC2RPlan struct {
 	Out       []float64
 }
 
-func (c2r DFTC2RPlan) String() string {
-	return fmt.Sprintf("{Kind:C2R Locality:%s PlanFlags:%s In:[%d]complex128 Out:[%d]float64}", c2r.Locality, c2r.PlanFlags, len(c2r.In), len(c2r.Out))
-}
-
+// Plans 1D Complex-to-Real DFT of size n. For in-place transforms input and
+// output arrays are backed by the same array using reflection. This is
+// inherently unsafe as it requires casting an array of one type to another
+// which is not allowed in the type system.
 func NewDFTC2R(n uint, locality Locality, planFlags PlanFlag) (plan DFTC2RPlan) {
 	plan.Locality = locality
 	plan.PlanFlags = planFlags
 
 	plan.In = make([]complex128, n>>1+1)
 
+	// Allocate the input and output arrays according to given locality. In-
+	// place transforms require reflection to point the input and output
+	// arrays to the same data since they are of different types.
 	switch {
 	case locality == InPlace:
 		header := *(*reflect.SliceHeader)(unsafe.Pointer(&plan.Out))
 		header.Len = int(n)
-		header.Cap = int(n)
+		header.Cap = header.Len
 		header.Data = uintptr(unsafe.Pointer(&plan.In[0]))
 		plan.Out = *(*[]float64)(unsafe.Pointer(&header))
 	case locality == OutOfPlace:
@@ -132,6 +153,7 @@ func NewDFTC2R(n uint, locality Locality, planFlags PlanFlag) (plan DFTC2RPlan) 
 		panic(fmt.Sprintf("invalid locality: %d\n", locality))
 	}
 
+	// Create the plan using the given arrays and flags.
 	plan.plan.p = C.fftw_plan_dft_c2r_1d(
 		C.int(n),
 		(*C.fftw_complex)(&plan.In[0]),
@@ -140,4 +162,8 @@ func NewDFTC2R(n uint, locality Locality, planFlags PlanFlag) (plan DFTC2RPlan) 
 	)
 
 	return
+}
+
+func (c2r DFTC2RPlan) String() string {
+	return fmt.Sprintf("{Kind:C2R Locality:%s PlanFlags:%s In:[%d]complex128 Out:[%d]float64}", c2r.Locality, c2r.PlanFlags, len(c2r.In), len(c2r.Out))
 }
